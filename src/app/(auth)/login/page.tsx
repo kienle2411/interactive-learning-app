ae58758c78d0262c57c42a9a755809e8f22a411b
SBPhungNguyen <22521148@gm.uit.edu.vn>
2024-11-27 18:07:32 +0700

    adjust login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,18 @@
+"use client";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
+import { useState } from "react";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    //do something here 
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
       <div className="flex flex-col md:flex-row w-full max-w-4xl bg-white rounded-lg shadow-md">
@@ -20,19 +29,19 @@ export default function LoginPage() {
           <div className="flex items-center justify-center mb-6">
             <h1 className="text-2xl font-bold text-green-600">LOG IN</h1>
           </div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="space-y-4">
               <div>
                 <Label htmlFor="email" className="mb-1 block text-sm font-medium text-gray-600">
                   Email
                 </Label>
-                <Input id="email" type="email" placeholder="Enter your email" className="w-full" />
+                <Input id="email" type="email" placeholder="Enter your email" className="w-full" value={email} onChange={(e) => setEmail(e.target.value)} />
               </div>
               <div>
                 <Label htmlFor="password" className="mb-1 block text-sm font-medium text-gray-600">
                   Password
                 </Label>
-                <Input id="password" type="password" placeholder="Enter your password" className="w-full" />
+                <Input id="password" type="password" placeholder="Enter your password" className="w-full" value={password} onChange={(e) => setPassword(e.target.value)} />
               </div>
 
             </div>
