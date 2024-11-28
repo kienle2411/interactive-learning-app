28c931eb679452e6a21de588ae83653383182065
SBPhungNguyen <22521148@gm.uit.edu.vn>
2024-11-28 19:48:58 +0700

    games pop up _ qr code

--- src/app/(page)/teacherGames/components/gameCard.tsx
+++ src/app/(page)/teacherGames/components/gameCard.tsx
@@ -2,8 +2,12 @@
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Copy, Play } from 'lucide-react'
 import React, { useState } from 'react'
+import QRCode from 'qrcode';
 
 interface GamesCardProps {
     id: string; // Unique ID for the class
@@ -15,6 +19,30 @@ interface GamesCardProps {
 // {id, nameOfClass, nameOfQuiz, questions}
 const GameCard: React.FC<GamesCardProps> = ({ id, nameOfClass, nameOfQuiz, questions, onDelete }) => {
     const [deleteConfirmOpen, setDeleteConfirmOpen] = useState(false);
+    const [randomCode, setRandomCode] = useState<string>('');
+
+    //qr code
+    const [qrCodeImage, setQrCodeImage] = useState<string | null>(null);
+
+    const handlePlay = async () => {
+        try {
+            //create random code
+            const array = new Uint32Array(1);
+            crypto.getRandomValues(array);
+            const code = (array[0] % 900000 + 100000).toString();
+            setRandomCode(code);
+            //create random code ends
+
+            const dataUrl = await QRCode.toDataURL(code.toString());
+            setQrCodeImage(dataUrl);
+        } catch (error) {
+            console.error('Error generating QR code:', error);
+        }
+    };
+    //qr code ends
+
+
+
 
     const handleDelete = () => {
         onDelete(id); // Gọi hàm onDelete khi xoá
@@ -46,10 +74,45 @@ const GameCard: React.FC<GamesCardProps> = ({ id, nameOfClass, nameOfQuiz, quest
                 </CardHeader>
                 {/* card-content */}
                 <CardContent className="pt-2 pb-2 pl-4 flex flex-row items-center justify-between">
-                    <h2 className='font-bold text-lg'>{nameOfClass}</h2>
-                    <button>
-                        <Play color='green' size={28} fill='green' />
-                    </button>
+                    <h2 className='font-bold text-base'>{nameOfClass}</h2>
+                    {/*  */}
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <button onClick={() => handlePlay()}>
+                                <Play color='green' size={28} fill='green' />
+                            </button>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-md">
+                            <DialogHeader>
+                                <DialogTitle>Let's start the Game!</DialogTitle>
+                                <DialogDescription>
+                                    Please enter or scan this code
+                                </DialogDescription>
+                            </DialogHeader>
+                            <div className="flex flex-col items-center space-x-2 ">
+                                <div className="grid flex-1 gap-2 w-full text-center pb-4">
+                                    <Label className='text-5xl font-semibold'>{randomCode}</Label>
+                                </div>
+                                {qrCodeImage && (
+                                    <img
+                                        src={qrCodeImage}
+                                        alt="QR Code"
+                                        className="max-w-[200px] mx-auto" />)}
+                            </div>
+                            <DialogFooter className="sm:justify-end">
+                                <DialogClose asChild>
+                                    <Button type="button" variant="secondary">
+                                        Close
+                                    </Button>
+                                </DialogClose>
+                                <Button type="button" variant="default">
+                                    Start
+                                </Button>
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
+                    {/*  */}
+
                     <div className='flex md:flex-col sm:flex-col lg:flex-row'>
                         <Button className='m-2'
                             variant="secondary"
