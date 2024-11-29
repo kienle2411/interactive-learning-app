169dff0af6190e4b63cc6cfeaf19d586bb65763c
SBPhungNguyen <22521148@gm.uit.edu.vn>
2024-11-30 06:24:48 +0700

    add studentGames

--- src/app/(stdpage)/studentGames/page.tsx
+++ src/app/(stdpage)/studentGames/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+"use client";
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import Link from 'next/link'
+import React, { useState } from 'react'
 
 export default function page() {
+    const [codeData, setCodeData] = useState('');
     return (
-        <div>page</div>
+        <div className='w-full flex flex-col items-center justify-center overflow-x-hidden overflow-y-hidden' style={{ height: 'calc(100vh - 10rem)' }}>
+            <div className='w-full flex flex-col items-center justify-center gap-4 overflow-x-hidden overflow-y-hidden'>
+                <span className='text-lg'>Enter the Game Code to join</span>
+                <Input className='w-80'
+                    placeholder='Enter Game Code'
+                    value={codeData}
+                    onChange={(e) => setCodeData(e.target.value)} />
+                <Button className='font-semibold text-base' style={{ backgroundColor: "#5048E5", color: "white" }}>Enter</Button>
+                <span>Or <Link href={""} className='text-green-600 underline'>Scan QR Code</Link></span>
+            </div>
+        </div>
     )
 }
