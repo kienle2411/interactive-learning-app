419a02755b3b430777b1236177358b7589465e29
Le Trung Kien <147020559+kienle2411@users.noreply.github.com>
2024-12-30 13:50:01 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:3001/api/:path*",
-      },
         {
         source: "/documentation/:path*",
         destination: "http://localhost:3001/documentation/:path*",
