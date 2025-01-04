be16a462c82a7620ef762ead4b7f9b26b4311bc6
Le Trung Kien <22520705@gm.uit.edu.vn>
2025-01-05 06:12:02 +0700

    feat: add play session student

--- src/app/(page)/teacherClasses/[id]/classSessions/[session-id]/play/page.tsx
+++ src/app/(page)/teacherClasses/[id]/classSessions/[session-id]/play/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import Cookies from "js-cookie";
 
 const socket = io("http://localhost:3001");
 
@@ -14,11 +15,20 @@ export default function SessionPlayPage() {
   const { "session-id": sessionId } = useParams();
   const [slide, setSlide] = useState(0);
   const [sessionMessage, setSessionMessage] = useState<string>("");
-
+  const [isStopped, setStopped] = useState(false);
   const id = Array.isArray(sessionId) ? sessionId[0] : sessionId;
   const { data: sessionData } = useGetSessionDetails(id || "");
   const presentationId = sessionData?.data?.presentationId;
   const { data: docFileData } = useGetDocFileDetails(presentationId ?? "");
+
+  useEffect(() => {
+    if (!isStopped) {
+      socket.emit("sendSlide", {
+        sessionId: id,
+        slideUrl: docFileData?.data?.url[slide],
+      });
+    }
+  }, [slide, isStopped]);
 
   function nextSlide() {
     if (
@@ -36,7 +46,10 @@ export default function SessionPlayPage() {
   }
 
   useEffect(() => {
-    socket.emit("joinSession", { sessionId: "session123", userId: "student1" });
+    socket.emit("joinSession", {
+      sessionId: id,
+      userId: "teacher",
+    });
 
     socket.on("sessionJoined", (data) => {
       setSessionMessage(data);
@@ -51,6 +64,16 @@ export default function SessionPlayPage() {
     };
   }, []);
 
+  function stopPresentation() {
+    setStopped(true);
+    socket.emit("stopPresentation", { sessionId: id });
+  }
+
+  function resumePresentation() {
+    setStopped(false);
+    socket.emit("resumePresentation", { sessionId: id });
+  }
+
   return (
     <div className="flex gap-5">
       <div className="basis-1/2">
@@ -62,7 +85,10 @@ export default function SessionPlayPage() {
           alt=""
         />
         <div className="flex justify-end gap-5 p-5">
-          <Button className="bg-red-500 hover:bg-red-800">
+          <Button
+            className="bg-red-500 hover:bg-red-800"
+            onClick={isStopped ? resumePresentation : stopPresentation}
+          >
             <StopCircle />
           </Button>
           <Button onClick={prevSlide} disabled={slide < 1}>
@@ -80,7 +106,7 @@ export default function SessionPlayPage() {
         </div>
         <div>{docFileData?.data?.url.length}</div>
       </div>
-      <div className="basis-1/2">Test</div>
+      <div className="basis-1/2">{/* Submit List */}</div>
     </div>
   );
 }
---
+++ src/app/(stdpage)/studentClasses/[id]/classSessions/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useGetSessionClassrooms } from "@/hooks/useSessions";
+import { useParams } from "next/navigation";
+import SessionCardMedium from "@/components/ui/session-card";
+
+export default function SessionTab() {
+  const { id } = useParams();
+  const classroomId = Array.isArray(id) ? id[0] : id;
+  const { data: sessions } = useGetSessionClassrooms(classroomId || "");
+  console.log(sessions);
+  return (
+    <div className="w-full">
+      {Array.isArray(sessions?.data?.data) ? (
+        sessions.data.data.map((session) => (
+          <SessionCardMedium session={session} />
+        ))
+      ) : (
+        <div>No sessions available</div>
+      )}
+    </div>
+  );
+}
